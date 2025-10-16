0d17104cfd3f48ff1fdd742b719d53578e4018ba
Saad Shabbir <126228618+saadii007@users.noreply.github.com>
2025-10-16 23:05:14 +0530

    Implement Redis caching for getCityAQI function
    Added caching for AQI data using Redis to improve performance.

--- src/services/aqiService.ts
+++ src/services/aqiService.ts
@@ -1,17 +1,23 @@
 import axios from "axios";
 import pool from "../config/db";
+import redisClient from "../config/redis";
 
 const API_KEY = process.env.OPENWEATHER_API_KEY;
 
 export async function getCityAQI(city: string) {
-  console.log("🔑 API Key in container:", process.env.OPENWEATHER_API_KEY);
   try {
-    const apiKey = process.env.OPENWEATHER_API_KEY;
-    const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+
+    const cached = await redisClient.get(city.toLowerCase());
+    if (cached) {
+      console.log("Cache hit for", city);
+      return JSON.parse(cached);
+    }
+
+    const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
     const geoResponse = await axios.get(geoUrl);
     const { lat, lon } = geoResponse.data[0];
 
-    const airUrl = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const airUrl = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
     const airResponse = await axios.get(airUrl);
 
     const data = airResponse.data.list[0];
@@ -22,23 +28,36 @@ export async function getCityAQI(city: string) {
       aqi === 3 ? "Moderate" :
       aqi === 4 ? "Poor" : "Very Poor";
 
+    const result = {
+      city,
+      aqi,
+      level,
+      components: data.components
+    };
+
     await pool.query(
       `INSERT INTO aqi_readings (city, aqi, level, co, no2, o3, so2, pm2_5, pm10, nh3)
        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
-      [city, aqi, level,
-       data.components.co,
-       data.components.no2,
-       data.components.o3,
-       data.components.so2,
-       data.components.pm2_5,
-       data.components.pm10,
-       data.components.nh3]
+      [
+        city,
+        aqi,
+        level,
+        data.components.co,
+        data.components.no2,
+        data.components.o3,
+        data.components.so2,
+        data.components.pm2_5,
+        data.components.pm10,
+        data.components.nh3
+      ]
     );
 
-    return { city, aqi, level, components: data.components };
+    await redisClient.setEx(city.toLowerCase(), 300, JSON.stringify(result));
+
+    return result;
 
   } catch (error: any) {
-    console.error("Error fetching AQI data:", error.message);
+    console.error("❌ Error fetching AQI data:", error.message);
     throw new Error("Failed to fetch AQI data");
   }
 }
