7d654881875e3d92e71319b88bfde9a032ca5cf3
Saad Shabbir <126228618+saadii007@users.noreply.github.com>
2025-10-16 23:09:21 +0530

    Log connection event for PostgreSQL
    Added connection event logging for PostgreSQL.

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,10 @@
 import { Pool } from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+pool.on("connect", () => console.log("📦 Connected to PostgreSQL"));
 export default pool;
